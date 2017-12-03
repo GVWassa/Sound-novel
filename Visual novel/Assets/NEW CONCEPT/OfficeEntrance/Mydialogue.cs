88ddcffcecff386bfaff4b71918f6c6dec8c7c49
GVWassa <libertysone@gmail.com>
2017-12-02 20:22:31 -0500

    I add a new system dialogue that worked
    new way of display images

---
+++ Visual novel/Assets/NEW CONCEPT/OfficeEntrance/Mydialogue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mydialogue : MonoBehaviour {
+
+	public float timer;
+	public GameObject[] buttons;
+	int buttonCounter;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer += Time.deltaTime;
+		
+	}
+
+	public void buttonClick(Object myObject){
+
+	if(timer > 100){
+					if(myObject == buttons[buttonCounter]){
+															Destroy(myObject);
+															buttonCounter++;
+															}
+		}
+	}
+}
---
+++ Visual novel/Assets/NEW CONCEPT/OfficeEntrance/MydialogueSystem_notUsed.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MydialogueSystem : MonoBehaviour {
+
+   public void Button_Click(){
+
+   	Debug.Log("Hello,World");
+
+   }
+}
+
+//Source:https://www.youtube.com/watch?v=BkQre52Y2eE
